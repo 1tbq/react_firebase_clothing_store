8b6c9af83257040b8630385154835d6dfadbac6f
tariq bashir <onetariq@gmail.com>
2020-04-30 14:37:25 +1000

    styled the home page and install the node-scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div className="App">
-<h1>helllo form app.js</h1>
+      <HomePage />
     </div>
   );
 }
