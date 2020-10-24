ea2b8039630b9e1311c9197dc90b3fc2de2cf595
tariq bashir <onetariq@gmail.com>
2020-10-24 14:27:34 +1100

    initial commit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,24 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { config } from './config';
 
-const config = {
-    apiKey: "AIzaSyCxcsSlcVD1_Ap43tMc2aCa4yRphQHJ6bI",
-    authDomain: "react-shop-back.firebaseapp.com",
-    databaseURL: "https://react-shop-back.firebaseio.com",
-    projectId: "react-shop-back",
-    storageBucket: "react-shop-back.appspot.com",
-    messagingSenderId: "416372786103",
-    appId: "1:416372786103:web:11edb3b85cf05cdd219e32",
-    measurementId: "G-3X8QSCMEF7"
-};
+
 
 export const createUserProfileDocument = async (userAuth, addionalData) => {
     if (!userAuth) return;
     const userRef = firestore.doc(`users/${userAuth.uid}`);
     const snapShot = await userRef.get();
-    if(!snapShot.exists){
-        const {displayName,email}= userAuth;
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
         const createAt = new Date();
         try {
             await userRef.set({
@@ -28,7 +20,7 @@ export const createUserProfileDocument = async (userAuth, addionalData) => {
                 ...addionalData
             })
         } catch (error) {
-            console.log('error creating user',error.message); 
+            console.log('error creating user', error.message);
         }
     }
     return userRef;
